71659b55502358e26c51e5ce1a72ae4beca5381a
Mohamad_187417 <freesyr45@gmail.com>
2026-04-30 17:19:03 +0300

    Add success message after creating database
    Added a success message after database creation.

--- create_database.py
+++ create_database.py
@@ -7,7 +7,7 @@ users = pd.read_excel("data_new/users.xlsx")
 products = pd.read_excel("data_new/products.xlsx")
 ratings = pd.read_excel("data_new/ratings.xlsx")
 behavior = pd.read_excel("data_new/behavior_15500.xlsx")
-ؤ
+
 users.to_sql("users", conn, if_exists="replace", index=False)
 products.to_sql("products", conn, if_exists="replace", index=False)
 ratings.to_sql("ratings", conn, if_exists="replace", index=False)
